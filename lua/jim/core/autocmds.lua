19e31a75da8e94422b4946160f68c3c4fa27937d
Jim Öhman <jim.ohm@outlook.com>
2024-11-19 01:59:22 +0100

    required some fixes

--- lua/jim/core/autocmds.lua
+++ lua/jim/core/autocmds.lua
@@ -7,8 +7,14 @@ autocmd("BufWritePre", {
     local buf = vim.api.nvim_get_current_buf()
     local filename = vim.api.nvim_buf_get_name(buf)
     local view = vim.fn.winsaveview()
+    -- Save the buffer contents
+    vim.api.nvim_buf_call(buf, function()
+      vim.cmd("silent write")
+    end)
+    -- Format the file
     vim.fn.system(string.format("black --quiet %s", filename))
-    vim.cmd("e!")
+    -- Reload the buffer and restore the view
+    vim.cmd("edit!")
     vim.fn.winrestview(view)
   end,
 })
