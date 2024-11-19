22fa364bf72c497887d1285a7da1b7ffd5276e58
Jim Öhman <jim.ohm@outlook.com>
2024-11-19 01:43:40 +0100

    add auto-format on save with black

--- lua/jim/core/autocmds.lua
+++ lua/jim/core/autocmds.lua
@@ -1,8 +1,14 @@
 local autocmd = vim.api.nvim_create_autocmd
 
 
--- autocmd('BufWritePost', {
---   pattern = '*.py',
---   command = ":Black"
--- })
-
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.py",
+  callback = function()
+    local buf = vim.api.nvim_get_current_buf()
+    local filename = vim.api.nvim_buf_get_name(buf)
+    local view = vim.fn.winsaveview()
+    vim.fn.system(string.format("black --quiet %s", filename))
+    vim.cmd("e!")
+    vim.fn.winrestview(view)
+  end,
+})
